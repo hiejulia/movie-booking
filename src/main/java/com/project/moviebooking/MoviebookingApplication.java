e2ec218d663991cd9f91328a0024c2fd246fc3fc
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-03-04 09:48:59 +0200

    update swagger

--- src/main/java/com/project/moviebooking/MoviebookingApplication.java
+++ src/main/java/com/project/moviebooking/MoviebookingApplication.java
@@ -1,9 +1,15 @@
 package com.project.moviebooking;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableSwagger2
+@Import({springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class})
 public class MoviebookingApplication {
 
 	public static void main(String[] args) {
